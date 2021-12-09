d172995b52556287397b8dbbf43cda6571ab24c5
Luca14797 <37103375+Luca14797@users.noreply.github.com>
2021-12-09 13:09:39 +0100

    Added datasets

--- src/generate_dataset.py
+++ src/generate_dataset.py
@@ -1,5 +1,7 @@
 from random import seed
 from random import randint
+import json
+
 
 def generate_id(n, dim_list):
     res = []
@@ -40,7 +42,7 @@ def generate_priority(dim_list):
 def main():
 
     o_r = 5
-    beta = 200
+    beta = 100
     s = 6
     n = o_r * beta
     dim_list = []
@@ -61,12 +63,15 @@ def main():
     time = generate_time(dim_list)
     priority = generate_priority(dim_list)
 
-    with open('Agnetis_data.dat', 'w') as f:
+    with open('datasets/Agnetis_' + str(o_r) + '_' + str(beta) + '.dat', 'w') as f:
         f.write('S = {"GYN", "GS", "ENT", "URO", "DS", "ORTH"};\n\n')
+        # f.write('J = {"OR1", "OR2", "OR3", "OR4", "OR5", "OR6", "OR7", "OR8", "OR9", "OR10"};\n\n')
+        # f.write('J = {"OR1", "OR2", "OR3", "OR4", "OR5", "OR6", "OR7", "OR8", "OR9", "OR10", "OR11", "OR12", "OR13", "OR14", "OR15"};\n\n')
         f.write('J = {"OR1", "OR2", "OR3", "OR4", "OR5"};\n\n')
         f.write('DAYS = {1, 2, 3, 4, 5};\n\n')
         f.write('N = %d;\n\n' %n)
-        f.write('I = %s;\n\n' %id)
+
+        f.write('I = %s;\n\n' %json.dumps(id))
         f.write('P = %s;\n\n' %time)
         f.write('R = %s;\n\n' %priority)
         f.write('NOT = 25;\n\n')
@@ -74,8 +79,17 @@ def main():
         f.write('S_min = [2, 6, 1, 1, 3, 6];\n\n')
         f.write('S_max = [6, 10, 5, 5, 7, 10];\n\n')
         f.write('PS = [1, 2, 1, 1, 1, 2];\n\n')
+        # f.write('NA = [{"OR1", "OR2", "OR4", "OR5", "OR6", "OR7", "OR9", "OR10"}, {"OR5", "OR10"}, {"OR1", "OR2", "OR4", "OR5", "OR6", "OR7", "OR9", "OR10"}, {"OR2", "OR3", "OR4", "OR5", "OR7", "OR8", "OR9", "OR10"}, {"OR5", "OR10"}, {"OR1", "OR2", "OR3", "OR6", "OR7", "OR8"}];\n\n')
+        # f.write('NA = [{"OR1", "OR2", "OR4", "OR5", "OR6", "OR7", "OR9", "OR10", "OR11", "OR12", "OR14", "OR15"}, {"OR5", "OR10", "OR15"}, {"OR1", "OR2", "OR4", "OR5", "OR6", "OR7", "OR9", "OR10", "OR11", "OR12", "OR14", "OR15"}, {"OR2", "OR3", "OR4", "OR5", "OR7", "OR8", "OR9", "OR10", "OR12", "OR13", "OR14", "OR15"}, {"OR5", "OR10", "OR15"}, {"OR1", "OR2", "OR3", "OR6", "OR7", "OR8", "OR11", "OR12", "OR13"}];\n\n')
         f.write('NA = [{"OR1", "OR2", "OR4", "OR5"}, {"OR5"}, {"OR1", "OR2", "OR4", "OR5"}, {"OR2", "OR3", "OR4", "OR5"}, {"OR5"}, {"OR1", "OR2", "OR3"}];\n\n')
         f.write('W = 90;\n\n')
+
+    k = 0
+    for i in range(s):
+        for j in range(len(time[i])):
+            k = k + (time[i][j] * (90 - priority[i][j]))
+    
+    print("Best: %s" %k)
 
 
 if __name__ == "__main__":
